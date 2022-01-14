3eaec504510630a9445f99911a58c62200399701
samaygo89 <75504344+samaygo89@users.noreply.github.com>
2022-01-14 14:41:06 -0500

    Samay's Name

--- src/main/java/edu/wpi/team3/Main.java
+++ src/main/java/edu/wpi/team3/Main.java
@@ -4,11 +4,10 @@ public class Main {
   public static void main(String[] args) {
 
     App.launch(App.class, args);
-    System.out.println("Samay Govani");
     System.out.println("Alex Sun");
     System.out.println("Aidan Burns");
     System.out.println("Randy Huang");
     System.out.println("Ethan Schrim");
-
+    System.out.println("Samay Govani");
   }
 }
