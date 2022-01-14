864ad01b0f87e389b247203f1361236c2725e74b
ETShrimpinator <jecschrim@gmail.com>
2022-01-14 14:58:40 -0500

    added println for Ethan

--- src/main/java/edu/wpi/team3/Main.java
+++ src/main/java/edu/wpi/team3/Main.java
@@ -11,20 +11,10 @@ public class Main {
     System.out.println("Aidan Burns");
     System.out.println("Randy Huang");
     System.out.println("Ethan Schrim");
-<<<<<<< HEAD
     System.out.println("Eli Hoffberg");
-  }
-}
-
-=======
     System.out.println("Robbie Eskridge");
-
-    aidanFunction();
-  }
-
-  private static void aidanFunction(){
     System.out.println("Branch - Aidan Burns");
+
   }
 
 }
->>>>>>> e39a3b83ce25818f6e8f25aafb186baf06a6c524
