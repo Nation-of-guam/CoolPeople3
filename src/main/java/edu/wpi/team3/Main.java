984450fec16e5e37cebdab812b1dbb3b58627b6d
Nation-of-guam <46681480+Nation-of-guam@users.noreply.github.com>
2022-01-14 14:58:02 -0500

    added function

--- src/main/java/edu/wpi/team3/Main.java
+++ src/main/java/edu/wpi/team3/Main.java
@@ -11,12 +11,11 @@ public class Main {
     System.out.println("Aidan Burns");
     System.out.println("Randy Huang");
     System.out.println("Ethan Schrim");
-<<<<<<< HEAD
-    System.out.println("Eli Hoffberg");
-  }
-}
 
-=======
+    System.out.println("Eli Hoffberg");
+
+
+
     System.out.println("Robbie Eskridge");
 
     aidanFunction();
@@ -27,4 +26,4 @@ public class Main {
   }
 
 }
->>>>>>> e39a3b83ce25818f6e8f25aafb186baf06a6c524
+
