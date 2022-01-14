c5d05aef50a45ef021c734555934305f42018c20
Nation-of-guam <46681480+Nation-of-guam@users.noreply.github.com>
2022-01-14 14:13:07 -0500

    Initial commit

---
+++ src/main/java/edu/wpi/team3/Main.java
@@ -0,0 +1,8 @@
+package edu.wpi.team3;
+
+public class Main {
+
+  public static void main(String[] args) {
+    App.launch(App.class, args);
+  }
+}
---
+++ src/test/java/edu/wpi/team3/DefaultTest.java
@@ -0,0 +1,13 @@
+/*-------------------------*/
+/* DO NOT DELETE THIS TEST */
+/*-------------------------*/
+
+package edu.wpi.team3;
+
+import org.junit.jupiter.api.Test;
+
+public class DefaultTest {
+
+  @Test
+  public void test() {}
+}
