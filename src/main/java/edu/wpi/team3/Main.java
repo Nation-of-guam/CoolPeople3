4c9496d9fe335a4ca615adc75867828c170bd9c5
Nation-of-guam <46681480+Nation-of-guam@users.noreply.github.com>
2022-01-14 14:38:11 -0500

    added function

--- src/main/java/edu/wpi/team3/Main.java
+++ src/main/java/edu/wpi/team3/Main.java
@@ -3,10 +3,15 @@ package edu.wpi.team3;
 public class Main {
 
   public static void main(String[] args) {
-    App.launch(App.class, args);
+    // App.launch(App.class, args);
 
     System.out.println("Alex Sun");
     System.out.println("Aidan Burns");
+
+    AidansFunction();
   }
 
+  private static void AidansFunction() {
+    System.out.println("Branch - Aidan Burns");
+  }
 }
